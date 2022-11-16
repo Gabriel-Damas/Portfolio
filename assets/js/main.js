3ce7d4bc69d3ff51e0ef5dc1619c3ab7b0073644
Damasceno <luambito93@gmail.com>
2022-11-16 14:19:17 -0300

    Colocando CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
-
+/*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
     const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
     if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
+/*=============== SERVICES MODAL ===============*/
 const modalViews = document.querySelectorAll('.services__modal'),
     modalBtns = document.querySelectorAll('.services__button'),
     modalClose = document.querySelectorAll('.services__modal-close');
@@ -27,7 +29,7 @@ modalClose.forEach((mc, i) => {
     })
 })
 
-
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
 let mixerPortfolio = mixitup('.work__container', {
     selectors: {
         target: '.work__card'
@@ -37,6 +39,7 @@ let mixerPortfolio = mixitup('.work__container', {
     }
 });
 
+/* Link active work */
 const linkwork = document.querySelectorAll('.work__item')
 
 function activeWork() {
@@ -45,6 +48,7 @@ function activeWork() {
 }
 linkwork.forEach(l => l.addEventListener('click', activeWork))
 
+/*=============== SWIPER TESTIMONIAL ===============*/
 let swiperTestimonial = new Swiper(".testimonial__container", {
     spaceBetween: 24,
     loop: true,
@@ -64,6 +68,7 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
     }
 });
 
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
@@ -84,35 +89,44 @@ function scrollActive() {
 window.addEventListener('scroll', scrollActive)
 
 
+/*=============== LIGHT DARK THEME ===============*/
 const themeButton = document.getElementById('theme-button')
 const lightTheme = 'light-theme'
 const iconTheme = 'bx-sun'
 
+// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
+// We obtain the current theme that the interface has by validating the light-theme class
 const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
 
+// We validate if the user previously chose a topic
 if (selectedTheme) {
-
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
     themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
 }
 
+// Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
     document.body.classList.toggle(lightTheme)
     themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
 
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2500,
     delay: 400,
-
+    // reset:true,
 })
 
 sr.reveal(`.home__data`)
